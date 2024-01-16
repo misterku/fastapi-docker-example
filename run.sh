c724c64a24173dd8f0221f3de58b6a796881f564
Aleksandr Kudriashov <misterku@yandex-team.ru>
2024-01-16 18:37:42 +0300

    dockerfile

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-uvicorn main:app --host 0.0.0.0 --port 9875 --reload & uvicorn main:app2 --host 0.0.0.0 --port 9873 --reload 
+uvicorn main:app --host 0.0.0.0 --port 9875 & uvicorn main:app2 --host 0.0.0.0 --port 9873 
